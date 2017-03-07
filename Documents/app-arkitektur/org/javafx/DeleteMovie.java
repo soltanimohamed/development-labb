e1eecc896affd53a1d8621043b0ef52b38d4b41d
Corvuscorone <krakan_krax@hotmail.com>
2017-03-07 12:40:37 +0100

    Working files?

--- Documents/app-arkitektur/org/javafx/DeleteMovie.java
+++ Documents/app-arkitektur/org/javafx/DeleteMovie.java
@@ -17,42 +17,43 @@ import src.domain.Storage;
 import src.domain.DBStorage;
 import src.domain.Actor;
 import src.domain.Movie;
-public class DeleteActor{
+
+public class DeleteMovie{
   private Storage storage;
-public DeleteActor(Storage storage){
-  this.storage = storage;
-  deleteA();
-}
-public void deleteA(){
-  Stage window = new Stage();
-  window.initModality(Modality.APPLICATION_MODAL);
-  window.setTitle("Delete Movie");
-  window.setMinWidth(200);
-  Label label = new Label("Enter name:");
-  label.setId("alert-text");
-  TextField titleField = new TextField();
-  Button okButton = new Button("ok");
-  okButton.setOnAction( e ->{
-    if(!(titleField.getText().equals(""))){
-      storage.deleteActor(titleField.getText());
-      window.close();
-    }
-  });
-  Button noButton = new Button("cancel");
-  noButton.setOnAction( e -> window.close());
-  GridPane pane = new GridPane();
-  pane.setVgap(10);
-  pane.setHgap(10);
-  pane.setPadding(new Insets(10,10,10,10));
-  pane.setConstraints(label,0,0);
-  pane.setConstraints(titleField,1,0);
-  pane.setConstraints(okButton,0,1);
-  pane.setConstraints(noButton,1,1);
+  public DeleteMovie(Storage storage){
+    this.storage = storage;
+    deleteA();
+  }
+  public void deleteA(){
+    Stage window = new Stage();
+    window.initModality(Modality.APPLICATION_MODAL);
+    window.setTitle("Delete Movie");
+    window.setMinWidth(200);
+    Label label = new Label("Enter name:");
+    label.setId("alert-text");
+    TextField titleField = new TextField();
+    Button okButton = new Button("ok");
+    okButton.setOnAction( e ->{
+      if(!(titleField.getText().equals(""))){
+        storage.deleteActor(titleField.getText());
+        window.close();
+      }
+    });
+    Button noButton = new Button("cancel");
+    noButton.setOnAction( e -> window.close());
+    GridPane pane = new GridPane();
+    pane.setVgap(10);
+    pane.setHgap(10);
+    pane.setPadding(new Insets(10,10,10,10));
+    pane.setConstraints(label,0,0);
+    pane.setConstraints(titleField,1,0);
+    pane.setConstraints(okButton,0,1);
+    pane.setConstraints(noButton,1,1);
     pane.getChildren().addAll(label,titleField,okButton,noButton);
-  pane.setAlignment(Pos.CENTER);
-  Scene scene = new Scene(pane);
-  scene.getStylesheets().add("javafx/style.css");
-  window.setScene(scene);
-  window.showAndWait();
-}
+    pane.setAlignment(Pos.CENTER);
+    Scene scene = new Scene(pane);
+    scene.getStylesheets().add("javafx/style.css");
+    window.setScene(scene);
+    window.showAndWait();
+  }
 }
