8fc0b7affe1d0be74702275bb040eb9362a76e43
Corvuscorone <krakan_krax@hotmail.com>
2017-03-09 11:58:33 +0100

    Changes to the delete funktion
    Combined deleteactor & deletemovie into deleteentryview. More classy ;)

--- Documents/app-arkitektur/org/javafx/MainWindow.java
+++ Documents/app-arkitektur/org/javafx/MainWindow.java
@@ -24,6 +24,7 @@ public class MainWindow extends Application{
   private Button submitButton;
   private Menu editMenu;
   private Menu fileMenu;
+  private Menu sortMenu;
   private MenuItem showAllmovies;
   private MenuItem showAllactors;
   private MenuItem addMovie;
@@ -31,6 +32,11 @@ public class MainWindow extends Application{
   private MenuItem addActor;
   private MenuItem deleteActor;
   private MenuItem exitApplication;
+  private MenuItem genre;
+  private MenuItem action;
+  private MenuItem comedy;
+  private MenuItem drama;
+  private MenuItem horror;
   private Button logginButton;
   private Button loggoutButton;
   private ListView<String> list;
@@ -43,48 +49,48 @@ public class MainWindow extends Application{
     launch(args);
   }
   public void start(Stage primaryStage) throws Exception{
-    Storage storage = new DBStorage();
-    primaryStage.setTitle("film-lager");
-    mainPane = new BorderPane();
-    pane1 = new GridPane();
-    pane2 = new GridPane();
-    pane4 = new VBox(20);
-    pane3 = new GridPane();
-    logginButton = new Button("Sign in");
-    logginButton.setOnAction( e ->{
-      SignIn.userLogin(storage);
-      if(storage.inlogg(SignIn.userField.getText(), SignIn.passField.getText()) != 0){
-        addMovie.setDisable(false);
-        addActor.setDisable(false);
-        deleteMovie.setDisable(false);
-        deleteActor.setDisable(false);
-        logginButton.setDisable(true);
-        loggoutButton.setDisable(false);
-      }
-    });
-    loggoutButton = new Button("Sign out");
+  Storage storage = new DBStorage();
+  primaryStage.setTitle("film-lager");
+  mainPane = new BorderPane();
+  pane1 = new GridPane();
+  pane2 = new GridPane();
+  pane4 = new VBox(20);
+  pane3 = new GridPane();
+  logginButton = new Button("Sign in");
+  logginButton.setOnAction( e ->{
+    SignIn.userLogin(storage);
+    if(storage.inlogg(SignIn.userField.getText(), SignIn.passField.getText()) != 0){
+     addMovie.setDisable(false);
+      addActor.setDisable(false);
+      deleteMovie.setDisable(false);
+      deleteActor.setDisable(false);
+      logginButton.setDisable(true);
+      loggoutButton.setDisable(false);
+  }
+  });
+  loggoutButton = new Button("Sign out");
+  loggoutButton.setDisable(true);
+  loggoutButton.setOnAction( e ->{
+    Alerts.display("Confirmation","you are now signed out");
+    logginButton.setDisable(false);
     loggoutButton.setDisable(true);
-    loggoutButton.setOnAction( e ->{
-      Alerts.display("Confirmation","you are now signed out");
-      logginButton.setDisable(false);
-      loggoutButton.setDisable(true);
-      addMovie.setDisable(true);
-      addActor.setDisable(true);
-      deleteMovie.setDisable(true);
-      deleteActor.setDisable(true);
-    });
-    list = new ListView<>();
-    pane2.setPadding(new Insets(10,10,10,10));
-    pane2.setVgap(8);
-    pane2.setHgap(10);
-    pane2.setConstraints(logginButton,0,0);
-    pane2.setConstraints(loggoutButton,0,1);
-    pane2.getChildren().addAll(logginButton,loggoutButton);
-    mainPane.setRight(pane2);
-    fileMenu = new Menu("File");
-    showAllmovies = new MenuItem("Show all movies");
-    showAllmovies.setOnAction( e -> {
-      List<Movie> allMovies = storage.showAllMovies();
+    addMovie.setDisable(true);
+     addActor.setDisable(true);
+     deleteMovie.setDisable(true);
+     deleteActor.setDisable(true);
+  });
+  list = new ListView<>();
+  pane2.setPadding(new Insets(10,10,10,10));
+  pane2.setVgap(8);
+  pane2.setHgap(10);
+  pane2.setConstraints(logginButton,0,0);
+  pane2.setConstraints(loggoutButton,0,1);
+  pane2.getChildren().addAll(logginButton,loggoutButton);
+  mainPane.setRight(pane2);
+  fileMenu = new Menu("File");
+  showAllmovies = new MenuItem("Show all movies");
+  showAllmovies.setOnAction( e -> {
+    List<Movie> allMovies = storage.showAllMovies();
       List<String> movies = new ArrayList<>();
       int i = 1;
       for(Movie m : allMovies){
@@ -95,10 +101,10 @@ public class MainWindow extends Application{
       for(String s : movies){
         list.getItems().add(s);
       }
-    });
-    showAllactors = new MenuItem("Show all actors");
-    showAllactors.setOnAction( e -> {
-      List<Actor> allActors = storage.showAllActors();
+  });
+  showAllactors = new MenuItem("Show all actors");
+  showAllactors.setOnAction( e -> {
+    List<Actor> allActors = storage.showAllActors();
       List<String> actors = new ArrayList<>();
       int i = 1;
       for(Actor a : allActors){
@@ -109,88 +115,118 @@ public class MainWindow extends Application{
       for(String s : actors){
         list.getItems().add(s);
       }
-    });
-    exitApplication = new MenuItem("Exit");
-    exitApplication.setOnAction( e -> primaryStage.close());
-    fileMenu.getItems().add(showAllmovies);
-    fileMenu.getItems().add(showAllactors);
-    fileMenu.getItems().add(new SeparatorMenuItem());
-    fileMenu.getItems().add(exitApplication);
-    editMenu = new Menu("Edit");
-    addMovie = new MenuItem("Add new movie...");
-    addMovie.setDisable(true);
-    deleteMovie = new MenuItem("Delete movie...");
-    deleteMovie.setDisable(true);
-    addActor = new MenuItem("Add new actor...");
-    addActor.setDisable(true);
-    deleteActor = new MenuItem("Delete actor");
-    deleteActor.setDisable(true);
-    editMenu.getItems().add(addMovie);
-    editMenu.getItems().add(deleteMovie);
-    editMenu.getItems().add(new SeparatorMenuItem());
-    editMenu.getItems().add(addActor);
-    editMenu.getItems().add(deleteActor);
-    //fileMenu.getItems().add(new SeperatorMenuItem());
-    MenuBar menubar = new MenuBar();
-    menubar.getMenus().addAll(fileMenu);
-    menubar.getMenus().addAll(editMenu);
-    mainPane.setLeft(pane3);
-    clearResultButton = new Button("Clear result");
-    clearResultButton.setOnAction( e ->list.getItems().clear());
-    submitButton = new Button("Submit");
-    pane1.setVgap(10);
-    pane1.setHgap(10);
-    pane1.setPadding(new Insets(10,10,10,250));
-    pane1.setConstraints(submitButton,0,0);
-    pane1.setConstraints(clearResultButton,1,0);
-    pane1.getChildren().addAll(submitButton,clearResultButton);
-    mainPane.setBottom(pane1);
-    mainPane.setCenter(list);
-    pane3.setPadding(new Insets(10,10,10,10));
-    pane3.setVgap(8);
-    pane3.setHgap(10);
-    searchField = new TextField();
-    searchField.setPromptText("movie or actor");
-    searchField.setOnAction( e ->{
-      String input =searchField.getText();
-      List<Movie> movies = storage.getMoviesByActorName(input);
-      List<Actor> actors = storage.getActorsByMovieTitle(input);
-      List<String> result = new ArrayList<>();
-      for(Actor a : actors){
-        result.add(a.name());
-      }
-      for(Movie m :movies){
-        result.add(m.title());
-      }
-      list.getItems().clear();
-      for(String s : result){
-        list.getItems().add(s);
-      }
-    });
-    pane3.setConstraints(searchField, 0, 0);
-    searchButton = new Button("search");
-    searchButton.setOnAction( e ->{
-      String input =searchField.getText();
-      List<Movie> movies = storage.getMoviesByActorName(input);
-      List<Actor> actors = storage.getActorsByMovieTitle(input);
-      List<String> result = new ArrayList<>();
-      for(Actor a : actors){
-        result.add(a.name());
-      }
-      for(Movie m :movies){
-        result.add(m.title());
-      }
-      list.getItems().clear();
-      for(String s : result){
-        list.getItems().add(s);
-      }
-    });
-    pane3.setConstraints(searchButton, 0, 1);
-    pane3.getChildren().addAll(searchField,searchButton);
-    mainPane.setTop(menubar);
-    Scene scene = new Scene(mainPane, 600,500);
-    scene.getStylesheets().add("javafx/style.css");
-    primaryStage.setScene(scene);
-    primaryStage.show();
+  });
+  exitApplication = new MenuItem("Exit");
+  exitApplication.setOnAction( e -> primaryStage.close());
+  fileMenu.getItems().add(showAllmovies);
+  fileMenu.getItems().add(showAllactors);
+  fileMenu.getItems().add(new SeparatorMenuItem());
+  fileMenu.getItems().add(exitApplication);
+  editMenu = new Menu("Edit");
+  addMovie = new MenuItem("Add new movie...");
+  addMovie.setDisable(true);
+  deleteMovie = new MenuItem("Delete movie...");
+  deleteMovie.setOnAction( e -> {
+    new DeleteEntryView(storage, "MOVIE");
+  });
+  deleteMovie.setDisable(true);
+  addActor = new MenuItem("Add new actor...");
+  addActor.setDisable(true);
+  deleteActor = new MenuItem("Delete actor");
+  deleteActor.setOnAction( e ->{
+    new DeleteEntryView(storage, "ACTOR");
+  });
+  deleteActor.setDisable(true);
+  editMenu.getItems().add(addMovie);
+  editMenu.getItems().add(deleteMovie);
+  editMenu.getItems().add(new SeparatorMenuItem());
+  editMenu.getItems().add(addActor);
+  editMenu.getItems().add(deleteActor);
+  sortMenu = new Menu("Sort");
+  action = new MenuItem("Action");
+  comedy = new MenuItem("Comedy");
+  drama = new MenuItem("Drama");
+  horror = new MenuItem("horror");
+  sortMenu.getItems().add(action);
+  sortMenu.getItems().add(comedy);
+  sortMenu.getItems().add(drama);
+  sortMenu.getItems().add(horror);
+  //sortMenu.getItems().add(new SeperatorMenuItem());
+  MenuBar menubar = new MenuBar();
+  menubar.getMenus().addAll(fileMenu);
+  menubar.getMenus().addAll(editMenu);
+  menubar.getMenus().addAll(sortMenu);
+  mainPane.setLeft(pane3);
+  clearResultButton = new Button("Clear result");
+  clearResultButton.setOnAction( e ->list.getItems().clear());
+  submitButton = new Button("Submit");
+  submitButton.setOnAction( e ->{
+    String message = "";
+    ObservableList<String> strings = list.getSelectionModel().getSelectedItems();
+     try{
+    for(String var : strings){
+      message = var;
+    }
+   String  mess = message.replaceAll("\\s+", "");
+    // Alerts.display("Exception", mess);
+    String adress = "https://www.youtube.com/results?search_query="+mess;
+    Alerts.openWebpage(new URL(adress));
+  }catch(Exception exc){
   }
+  });
+  pane1.setVgap(10);
+  pane1.setHgap(10);
+  pane1.setPadding(new Insets(10,10,10,250));
+  pane1.setConstraints(submitButton,0,0);
+  pane1.setConstraints(clearResultButton,1,0);
+  pane1.getChildren().addAll(submitButton,clearResultButton);
+  mainPane.setBottom(pane1);
+  mainPane.setCenter(list);
+  pane3.setPadding(new Insets(10,10,10,10));
+  pane3.setVgap(8);
+  pane3.setHgap(10);
+  searchField = new TextField();
+  searchField.setPromptText("movie or actor");
+  searchField.setOnAction( e ->{
+    String input =searchField.getText();
+       List<Movie> movies = storage.getMoviesByActorName(input);
+       List<Actor> actors = storage.getActorsByMovieTitle(input);
+       List<String> result = new ArrayList<>();
+       for(Actor a : actors){
+         result.add(a.name());
+       }
+       for(Movie m :movies){
+         result.add(m.title());
+       }
+       list.getItems().clear();
+       for(String s : result){
+         list.getItems().add(s);
+       }
+  });
+  pane3.setConstraints(searchField, 0, 0);
+  searchButton = new Button("search");
+  searchButton.setOnAction( e ->{
+    String input =searchField.getText();
+    List<Movie> movies = storage.getMoviesByActorName(input);
+    List<Actor> actors = storage.getActorsByMovieTitle(input);
+    List<String> result = new ArrayList<>();
+    for(Actor a : actors){
+      result.add(a.name());
+    }
+    for(Movie m :movies){
+      result.add(m.title());
+    }
+    list.getItems().clear();
+    for(String s : result){
+      list.getItems().add(s);
+    }
+  });
+  pane3.setConstraints(searchButton, 0, 1);
+  pane3.getChildren().addAll(searchField,searchButton);
+  mainPane.setTop(menubar);
+  Scene scene = new Scene(mainPane, 600,500);
+  scene.getStylesheets().add("javafx/style.css");
+  primaryStage.setScene(scene);
+  primaryStage.show();
+}
 }
