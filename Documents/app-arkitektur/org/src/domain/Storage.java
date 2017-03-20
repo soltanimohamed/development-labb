8d8f495a1f0403503d21e7f4393deacc82a6e1be
Corvuscorone <krakan_krax@hotmail.com>
2017-03-20 09:50:17 +0100

    New storage-file

--- Documents/app-arkitektur/org/src/domain/Storage.java
+++ Documents/app-arkitektur/org/src/domain/Storage.java
@@ -1,5 +1,7 @@
 package src.domain;
+
 import java.util.List;
+
 public interface Storage{
   public void addMovie(Movie m);
   public void addActor(Actor a);
@@ -7,7 +9,9 @@ public interface Storage{
   public void deleteActor(String name);
   public List<Movie> showAllMovies();
   public List<Actor> showAllActors();
+  public List<Movie> getMovieByTitle(String title);
+  public List<Actor> getActorByName(String name);
   public List<Movie> getMoviesByActorName(String actorName);
   public List<Actor> getActorsByMovieTitle(String title);
-  public int inlogg(String username, String password);
+  public int logIn(String username, String password);
 }
