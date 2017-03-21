24d6463b61acd1f29d48ace4f496c41985b76913
soltanimohamed <mohamed_soltani@live.com>
2017-03-21 10:31:09 +0100

    new movie.java

--- Documents/app-arkitektur/org/src/domain/Movie.java
+++ Documents/app-arkitektur/org/src/domain/Movie.java
@@ -1,40 +1,29 @@
 package src.domain;
-
 public class Movie{
   private String title;
   private String genre;
   private String regi;
   private String origin;
   private int year;
-
   public Movie(String title,String genre){
     this.title = title;
     this.genre = genre;
   }
-
   public Movie(String title, String genre, int year, String regi, String origin){
     this(title,genre);
     this.year = year;
     this.regi = regi;
     this.origin = origin;
   }
-
   public String title(){ return title; }
-
   public String genre(){ return genre; }
-
   public int year(){ return year; }
-
   public String regi(){ return regi; }
-
   public String origin(){ return origin; }
-
-  public void setTitle(String title){this.title = title;}
-
-  public void setGenre(String genre){this.genre = genre;}
-
-  public String toString(){
-    return title + ": " + genre + ": " + year + ": " + regi + ": " + origin;
-  }
+ public void setTitle(String title){this.title = title;}
+ public void setGenre(String genre){this.genre = genre;}
+ public String toString(){
+   return title + ": " + genre + ": " + year + ": " + regi + ": " + origin;
+ }
 
 }
